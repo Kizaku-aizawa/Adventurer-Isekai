f297c81a1149c5e604e7bbc7b046e3ebbefed6b7
Kizaku-aizawa <gaming7887over@gmail.com>
2025-11-30 09:35:07 +0700

    Add useGameLogic hook for game state management

---
+++ src/Hooks/useGameLogic.js
@@ -0,0 +1,8 @@
+export default function useGameLogic() {
+  // nanti berisi state game: player, combat, spawn, dll.
+  return {
+    // contoh API:
+    getPlayer: () => ({}),
+    attack: () => {},
+  }
+}
